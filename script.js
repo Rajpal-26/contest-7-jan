07b9df5b7ba398aa345019679c05c069e2774ed5
Rajpal-26 <rajpaltanwar2608@gmail.com>
2023-01-07 17:35:30 +0530

    contest

--- script.js
+++ script.js
@@ -8,20 +8,43 @@ let arr = [
 
 function PrintDeveloperswithMap() {
   //Write your code here
+  arr.map(item => {
+    if (item.profession == "developer")
+      console.log(item);
+  })
+
 }
 
 function PrintDeveloperbyForEach() {
   //Write your code here
+  arr.forEach((item) => {
+  console.log(arr);
+})
 }
 
 function addData() {
   //Write your code here
+  arr.push({ id: 4, name: "susan", age: "20", profession: "intern" })
+  console.log(arr)
 }
 
 function removeAdmin() {
   //Write your code here
+  arr.filter((item) => {
+    if (item.profession != "admin")
+      console.log(item)
+})
 }
 
 function concatenateArray() {
   //Write your code here
+  let arr2 = [
+  { id: 5, name: "ravi", age: "25", profession: "testing" },
+  { id: 6, name: "kishan", age: "17", profession: "Data scientist" },
+  { id: 7, name: "robert", age: "22", profession: "HR" },
+];
+
+  let combine = arr.concat(arr2);
+  console.log(combine);
+  
 }
